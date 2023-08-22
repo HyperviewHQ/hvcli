8198f83fc703026a72f52f8ba3f77fd9e639f173
Rami Jebara <rami.jebara@hyperviewhq.com>
2023-08-22 14:57:48 -0700

    added config parsing test

--- ait/src/hyperview/cli.rs
+++ ait/src/hyperview/cli.rs
@@ -26,6 +26,8 @@ pub fn get_config_path() -> String {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::Write;
+    use tempfile::NamedTempFile;
 
     #[test]
     fn test_get_config_path() {
@@ -39,5 +41,31 @@ mod tests {
         );
 
         assert_eq!(config_path, expected_path);
+    }
+
+    #[test]
+    fn test_app_config_loading() {
+        let mut tmp_file = NamedTempFile::new().unwrap();
+
+        write!(
+            tmp_file,
+            r#"client_id = "test_id"
+            client_secret = "test_secret"
+            scope = "test_scope"
+            auth_url = "https://test_auth_url"
+            token_url = "https://test_token_url"
+            instance_url = "https://test_instance_url"
+            "#
+        )
+        .unwrap();
+
+        let config: AppConfig =
+            confy::load_path(tmp_file.path().to_str().unwrap().to_string()).unwrap();
+        assert_eq!(config.client_id, "test_id");
+        assert_eq!(config.client_secret, "test_secret");
+        assert_eq!(config.scope, "test_scope");
+        assert_eq!(config.auth_url, "https://test_auth_url");
+        assert_eq!(config.token_url, "https://test_token_url");
+        assert_eq!(config.instance_url, "https://test_instance_url");
     }
 }
